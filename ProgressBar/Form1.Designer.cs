fb4be94da2897c1c84a591fa664aa56e43e66ac1
Jinju Kim <swinging13@naver.com>
2019-06-14 14:48:02 +0900

    초기 커밋.

---
+++ ProgressBar/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace ProgressBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbStatus = new System.Windows.Forms.ProgressBar();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // pbStatus
+            // 
+            this.pbStatus.ForeColor = System.Drawing.Color.CadetBlue;
+            this.pbStatus.Location = new System.Drawing.Point(12, 12);
+            this.pbStatus.Name = "pbStatus";
+            this.pbStatus.Size = new System.Drawing.Size(238, 23);
+            this.pbStatus.TabIndex = 0;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblStatus.Location = new System.Drawing.Point(256, 12);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(87, 59);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "상태 : ";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnRun
+            // 
+            this.btnRun.BackColor = System.Drawing.Color.DarkRed;
+            this.btnRun.ForeColor = System.Drawing.Color.White;
+            this.btnRun.Location = new System.Drawing.Point(13, 42);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 2;
+            this.btnRun.Text = "실행";
+            this.btnRun.UseVisualStyleBackColor = false;
+            this.btnRun.Click += new System.EventHandler(this.BtnRun_Click);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.AccessibleRole = System.Windows.Forms.AccessibleRole.SplitButton;
+            this.btnEnd.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.btnEnd.Location = new System.Drawing.Point(94, 42);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(75, 23);
+            this.btnEnd.TabIndex = 3;
+            this.btnEnd.Text = "멈춤";
+            this.btnEnd.UseVisualStyleBackColor = false;
+            this.btnEnd.Click += new System.EventHandler(this.BtnEnd_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Gold;
+            this.btnReset.Location = new System.Drawing.Point(175, 42);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "리셋";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // Timer
+            // 
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 80);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.pbStatus);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "행상태진";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar pbStatus;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Timer Timer;
+    }
+}
+
---
+++ ProgressBar/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgressBar
+{
+    public partial class Form1 : Form
+    {
+        int Num = 0;    // 진행숫자
+        string OrgStr = ""; // 상태 : 저장 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.OrgStr = this.lblStatus.Text;
+        }
+
+        private void BtnRun_Click(object sender, EventArgs e)
+        {
+            this.Timer.Enabled = true;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            this.Num++;
+            if (this.Num > 100)
+            {
+                this.Timer.Enabled = false;
+                return;
+            }
+            this.pbStatus.Value = Num;
+            this.lblStatus.Text = OrgStr + Num + "%";
+        }
+
+        private void BtnEnd_Click(object sender, EventArgs e)
+        {
+            this.Timer.Enabled = false;
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            this.Num = 0;
+            this.pbStatus.Value = Num;
+            this.lblStatus.Text = OrgStr;
+        }
+    }
+}
+
